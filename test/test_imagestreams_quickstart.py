de7abfd0696c372ceafc8470066a752cca957771
Petr "Stone" Hracek <phracek@redhat.com>
2024-06-24 14:09:20 +0200

    Add PyTest suite for testing nodejs container in OpenShift 4
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_imagestreams_quickstart.py
@@ -0,0 +1,83 @@
+import os
+import sys
+
+import pytest
+
+from container_ci_suite.openshift import OpenShiftAPI
+from container_ci_suite.utils import check_variables
+
+
+if not check_variables():
+    print("At least one variable from IMAGE_NAME, OS, SINGLE_VERSION is missing.")
+    sys.exit(1)
+
+
+VERSION = os.getenv("SINGLE_VERSION")
+IMAGE_NAME = os.getenv("IMAGE_NAME")
+OS = os.getenv("OS")
+
+DEPLOYED_PGSQL_IMAGE = "quay.io/sclorg/postgresql-15-c9s"
+
+NODEJS_TAGS = {
+    "rhel8": "-ubi8",
+    "rhel9": "-ubi9"
+}
+NODEJS_TAG = NODEJS_TAGS.get(OS, None)
+PGSQL_IMAGE_TAG = f"postgresql:15-c9s"
+IMAGE_TAG = f"15-c9s"
+
+
+# Replacement with 'test_python_s2i_templates'
+class TestImagestreamsQuickstart:
+
+    def setup_method(self):
+        self.oc_api = OpenShiftAPI(pod_name_prefix="nodejs-example", version=VERSION)
+        assert self.oc_api.upload_image(DEPLOYED_PGSQL_IMAGE, PGSQL_IMAGE_TAG)
+
+    def teardown_method(self):
+        self.oc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "template",
+        [
+            "nodejs.json",
+            "nodejs-postgresql-persistent.json",
+        ]
+    )
+    def test_nodejs_template_inside_cluster(self, template):
+        new_version = VERSION
+        if "minimal" in VERSION:
+            new_version = VERSION.replace("-minimal", "")
+        service_name = "nodejs-example"
+        template_url = self.oc_api.get_raw_url_for_json(
+            container="nodejs-ex", dir="openshift/templates", filename=template, branch="master"
+        )
+        openshift_args = [
+            f"SOURCE_REPOSITORY_URL=https://github.com/sclorg/nodejs-ex.git",
+            f"SOURCE_REPOSITORY_REF=master",
+            f"NODEJS_VERSION={new_version}",
+            f"NAME={service_name}"
+        ]
+        if template != "nodejs.json":
+            openshift_args = [
+                f"SOURCE_REPOSITORY_URL=https://github.com/sclorg/nodejs-ex.git",
+                f"SOURCE_REPOSITORY_REF=master",
+                f"POSTGRESQL_VERSION={IMAGE_TAG}",
+                f"NODEJS_VERSION={new_version}",
+                f"NAME={service_name}",
+                f"DATABASE_USER=testu",
+                f"DATABASE_PASSWORD=testpwd",
+                f"DATABASE_ADMIN_PASSWORD=testadminpwd"
+            ]
+        assert self.oc_api.imagestream_quickstart(
+            imagestream_file="imagestreams/nodejs-rhel.json",
+            template_file=template_url,
+            image_name=IMAGE_NAME,
+            name_in_template="nodejs",
+            openshift_args=openshift_args
+        )
+        assert self.oc_api.template_deployed(name_in_template=service_name)
+        assert self.oc_api.check_response_inside_cluster(
+            name_in_template=service_name, expected_output="Node.js Crud Application"
+        )
+
---
+++ test/test_nodejs_s2i_standalone.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import pytest
+
+from container_ci_suite.utils import check_variables
+from container_ci_suite.openshift import OpenShiftAPI
+
+if not check_variables():
+    print("At least one variable from IMAGE_NAME, OS, SINGLE_VERSION is missing.")
+    sys.exit(1)
+
+
+VERSION = os.getenv("SINGLE_VERSION")
+IMAGE_NAME = os.getenv("IMAGE_NAME")
+OS = os.getenv("TARGET")
+
+
+class TestNodeJSExTemplate:
+
+    def setup_method(self):
+        self.oc_api = OpenShiftAPI(pod_name_prefix="nodejs-testing", version=VERSION)
+
+    def teardown_method(self):
+        self.oc_api.delete_project()
+
+    def test_nodejs_ex_template_inside_cluster(self):
+        service_name = "nodejs-testing"
+        assert self.oc_api.deploy_s2i_app(
+            image_name=IMAGE_NAME, app=f"https://github.com/sclorg/s2i-nodejs-container.git",
+            context="test/test-app",
+            service_name=service_name
+        )
+        assert self.oc_api.template_deployed(name_in_template=service_name)
+        assert self.oc_api.check_response_inside_cluster(
+            name_in_template=service_name, expected_output="This is a node.js echo service"
+        )
