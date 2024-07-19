7c8596b19e0a7d029c2135dee4f63e7015e30abc
Petr "Stone" Hracek <phracek@redhat.com>
2024-07-19 13:35:21 +0200

    Use VERSION instead of SINGLE_VERSION like we do in whole org
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_latest_imagestreams.py
+++ test/test_latest_imagestreams.py
@@ -9,10 +9,10 @@ from container_ci_suite.utils import check_variables
 TEST_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
 
 if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, SINGLE_VERSION is missing.")
+    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
     sys.exit(1)
 
-VERSION = os.getenv("SINGLE_VERSION")
+VERSION = os.getenv("VERSION")
 
 
 class TestLatestImagestreams:
