3bb0ce4a50df3cd0bd7b37c1d2ecae6c4ed3e5aa
Petr "Stone" Hracek <phracek@redhat.com>
2025-05-15 10:12:07 +0200

    Add support for the latest imagestreams in RHEL10.
    PyTest and OpeenShift 4 tests are updated as well.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_shared_helm_nodejs_imagestreams.py
+++ test/test_shared_helm_nodejs_imagestreams.py
@@ -31,6 +31,8 @@ class TestHelmRHELNodeJSImageStreams:
     @pytest.mark.parametrize(
         "version,registry",
         [
+            ("22-ubi10", "registry.redhat.io/ubi10/nodejs-22:latest"),
+            ("22-ubi10-minimal", "registry.redhat.io/ubi10/nodejs-22-minimal:latest"),
             ("22-ubi9", "registry.redhat.io/ubi9/nodejs-22:latest"),
             ("22-ubi9-minimal", "registry.redhat.io/ubi9/nodejs-22-minimal:latest"),
             ("20-ubi9", "registry.redhat.io/ubi9/nodejs-20:latest"),
