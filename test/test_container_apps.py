0d9ce4df12f8fde843757c53cf9c95f363f0a1f1
Petr "Stone" Hracek <phracek@redhat.com>
2026-01-19 14:07:04 +0100

    Add support NPM_TOKEN for successfully downloading npm packages from private NPM repositories.
    The pull request also contains test suite as for
    BASH as for PyThon.
    
    See Jira issue: https://issues.redhat.com/browse/RHEL-113826
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_container_apps.py
+++ test/test_container_apps.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 from pathlib import Path
 
@@ -463,30 +464,58 @@ class TestNodeJSAppsHWContainer:
         assert "redacted" in podman_log_file
 
 
-class TestNodeJSIncrementalAppContainer:
+class TestNodeJSNOMAuthenticationTokenAppContainer:
     """
-    Test incremental build of a NodeJS application.
+    Test npm authentication token of a NodeJS application.
     """
 
     def setup_method(self):
         """
         Setup the test environment.
         """
-        self.build1 = build_s2i_app(test_incremental)
-        self.build2 = build_s2i_app(test_incremental, container_args="--incremental")
+        self.s2i_app = build_s2i_app(
+            test_app, container_args="-e NPM_TOKEN=some-token-for-testing"
+        )
 
     def teardown_method(self):
         """
         Cleanup the test environment.
         """
-        self.build1.cleanup()
-        self.build2.cleanup()
+        self.s2i_app.cleanup()
 
-    def test_incremental_build(self):
+    def test_npm_authentication_token(self):
         """
-        Test incremental build of a NodeJS application.
+        Test npm authentication token of a NodeJS application.
         """
-        skip_for_minimal()
-        build_log1 = self.build1.get_podman_build_log_file()
-        build_log2 = self.build2.get_podman_build_log_file()
-        assert build_log1 != build_log2
+        assert self.s2i_app.create_container(
+            cid_file_name=self.s2i_app.app_name,
+            container_args="-e NPM_TOKEN=some-token-for-testing",
+        )
+        cip, cid = self.s2i_app.get_cip_cid(cid_file_name=self.s2i_app.app_name)
+        assert cip, cid
+        assert self.s2i_app.test_response(url=f"http://{cip}")
+        INTERNAL_NPM_REGISTRY = (
+            os.getenv("NPM_MIRROR").replace("https://", "")
+            if os.getenv("NPM_MIRROR")
+            else "registry.npmjs.org"
+        )
+        NPM_REGISTRY_AUTH = f"//{INTERNAL_NPM_REGISTRY}:_auth"
+        npm_config_list = PodmanCLIWrapper.podman_run_command_and_remove(
+            cid_file_name=f"{VARS.IMAGE_NAME}-{self.s2i_app.app_name}",
+            cmd="npm config list",
+        )
+        pattern = rf"{NPM_REGISTRY_AUTH}\s*=\s*\(protected\)"
+        for line in npm_config_list.split("\n"):
+            if re.search(pattern, line):
+                print("Line found")
+                break
+        else:
+            assert False, (
+                f"Auth registry {NPM_REGISTRY_AUTH} not found in npm config list"
+            )
+        npmrc_file = PodmanCLIWrapper.podman_run_command_and_remove(
+            cid_file_name=f"{VARS.IMAGE_NAME}-{self.s2i_app.app_name}",
+            cmd="cat .npmrc",
+        ).strip()
+        pattern_file = rf"{NPM_REGISTRY_AUTH}=some-token-for-testing"
+        assert re.search(pattern_file, npmrc_file)
